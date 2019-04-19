6a26c5555f9a1de6acb514e8bd191b1b8f9c321a
neumayr <neumayr@users.noreply.github.com>
2019-04-19 13:23:33 +0200

    setup iOS Unit Testing

---
+++ sage9to5Tests/Sage9to5Tests.swift
@@ -0,0 +1,33 @@
+//
+//  sage9to5Tests.swift
+//  sage9to5Tests
+//
+//  Created by Matthias Neumayr on 19.04.19.
+//  Copyright © 2019 Matthias Neumayr. All rights reserved.
+//
+
+import XCTest
+
+class Sage9to5Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
